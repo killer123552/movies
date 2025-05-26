1fc53679485f03daadc15f8fb540fb6a3ec136de
killer123552 <killerxvipr@gmail.com>
2025-05-26 22:01:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,12 +15,12 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25895175'))
 API_HASH = environ.get('API_HASH', '6e455b91ca68d245e378aeb903afd001')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7410376741:AAH5opwvam3VIGPUkiWfSmSxlWEgAxXuYr8')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1728951776').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/KILLERxVIPP") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002566713244'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002250988240'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/al_movie_group_REQUEST')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002248710429').split()]
 #---------------------------------------------------------------
