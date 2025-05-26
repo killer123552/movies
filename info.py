7ddce26dba593d25de0bfb02e095a27ceda8a69c
killer123552 <killerxvipr@gmail.com>
2025-05-26 16:04:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7410376741:AAH5opwvam3VIGPUkiWfSmSxlWEgAxX
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1728951776').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/KILLERxVIPP") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002250988240'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002566713244'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/al_movie_group_REQUEST')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002248710429').split()]
 #---------------------------------------------------------------
